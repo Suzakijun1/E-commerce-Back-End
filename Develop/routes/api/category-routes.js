78dc82b19f82215fa6aab32bf849a82a5b3a66a4
suzakijun1 <dyong1997@gmail.com>
2023-05-03 12:11:24 -0400

    finished id categoryRoute

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -28,6 +28,26 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: {
+      model: Product,
+      attributes: ["id", "product_name", "price", "stock", "category_id"],
+    },
+  })
+    .then((dbCategory) => {
+      if (!dbCategory) {
+        res.status(404).json({ message: "no categories found" });
+        return;
+      }
+      res.json(dbCategory);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post("/", (req, res) => {
