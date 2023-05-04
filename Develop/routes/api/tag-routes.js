a6a05d4e857d2b5ddcfe4a1beb407cafb0273de0
suzakijun1 <dyong1997@gmail.com>
2023-05-04 12:19:54 -0400

    tag-route delete done

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -80,6 +80,17 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((tagRemoved) => {
+      res.json(`${tagRemoved} tag were removed from the database`);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 module.exports = router;
